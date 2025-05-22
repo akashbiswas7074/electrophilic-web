fa92f438120d70512b258ef79bf4615f03782c47
akashbiswas7074 <abworkhouse01@gmail.com>
2025-05-22 18:03:28 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,8 @@ import 'react-toastify/dist/ReactToastify.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'PAUL CO | Premium E-Store',
-  description: 'Premium shopping experience from PAUL CO',
+  title: 'Electrophilic',
+  description: 'Welcome to Electrophilic.in – Where Innovation Begins!Step into the world of electronics with the ultimate destination for makers, tinkerers, and tech enthusiasts. At Electrophicil.in, we bring you everything from microcontrollers and sensors to transistors, ICs, resistors, modules, and more—all in one place.Whether youre building your first DIY project or developing complex electronic systems, weve got the components you need to power your imagination. From on-board computers to the tiniest resistor, every part is carefully selected to help you create, experiment, and innovate without limits. Fast shipping, reliable quality, and unbeatable prices—this is more than a store, it’s your partner in every project. Dive into the world of electronics today with Electrophilic.in and bring your ideas to life!',
 };
 
 export default function RootLayout({
